8f149b4a402949ce59c297245df801e89aa9afff
cesaradrian18 <141053368+cesaradrian18@users.noreply.github.com>
2025-05-01 01:30:54 -0600

    Proyecto limpio con Hola mundo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,8 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Mi sitio Gatsby`,
+    author: `César Gómez`, // 👈 agrega esta línea
+    siteUrl: `https://www.ejemplo.com`,
   },
-  plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        defaults: {
-          formats: [`auto`, `webp`], // quitamos 'avif'
-        },
-      },
-    },
-    
-  ],
+  plugins: [],
 }
