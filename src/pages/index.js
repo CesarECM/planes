5ad2dff4fdedd5b3f0b6cf440e960a23b48e699f
cesaradrian18 <141053368+cesaradrian18@users.noreply.github.com>
2025-05-01 10:17:04 -0600

    Corrige importación del componente Header con mayúscula

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Header from "../components/Header"
+import Header from "../components/header"
 import Hero from "../components/Hero"
 import Features from "../components/Features"
 import Testimonials from "../components/Testimonials"
